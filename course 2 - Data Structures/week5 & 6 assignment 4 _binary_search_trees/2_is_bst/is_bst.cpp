741ca1e3b392972089d31ae02c7bb01af0d6aded
Prachi Vishnoi <43631660+prachivishnoi27@users.noreply.github.com>
2020-07-11 18:18:33 +0530

    Update is_bst.cpp
    Implementation of is_bst function.

--- course 2 - Data Structures/week5 & 6 assignment 4 _binary_search_trees/2_is_bst/is_bst.cpp
+++ course 2 - Data Structures/week5 & 6 assignment 4 _binary_search_trees/2_is_bst/is_bst.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using std::cin;
 using std::cout;
@@ -16,24 +17,36 @@ struct Node {
   Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
 };
 
-bool IsBinarySearchTree(const vector<Node>& tree) {
+vector<Node> tree;
+
+bool IsBinarySearchTree(Node p, int min, int max) {
   // Implement correct algorithm here
-  return true;
+  if (p.key < min || p.key > max)  
+        return false;
+  if(p.left == -1 && p.right == -1) return true;
+  else if(p.left == -1) return IsBinarySearchTree(tree[p.right], p.key+1, max);
+  else if(p.right == -1) return IsBinarySearchTree(tree[p.left], min, p.key-1);
+  else return
+        IsBinarySearchTree(tree[p.left], min, p.key-1) &&
+        IsBinarySearchTree(tree[p.right], p.key+1, max);
 }
 
 int main() {
   int nodes;
   cin >> nodes;
-  vector<Node> tree;
+  if(nodes == 0){
+    cout<<"CORRECT\n";
+    return 0;
+  }
   for (int i = 0; i < nodes; ++i) {
     int key, left, right;
     cin >> key >> left >> right;
     tree.push_back(Node(key, left, right));
   }
-  if (IsBinarySearchTree(tree) {
+  if (IsBinarySearchTree(tree[0], INT_MIN, INT_MAX))
     cout << "CORRECT" << endl;
-  } else {
+  else
     cout << "INCORRECT" << endl;
-  }
   return 0;
 }
+
